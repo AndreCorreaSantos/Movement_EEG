d0b077c692089d5c2363bbcd28efefc87940ca0f
AndreCorreaSantos <acorreasantos2002@gmail.com>
2023-12-08 19:36:52 -0300

    adding new version

--- models.py
+++ models.py
@@ -27,27 +27,27 @@ def recurrent_2(features, lstm_units=64, dropout_rate=0.5):
     return model
 
 
-def wavenet(filters=32, kernel_size=2, dilation_rates=(1, 2, 4, 8) * 2, batch_size=512):
+def wavenet(filters=32, kernel_size=2, dilation_rates=(1, 2, 4, 8) * 2,n_steps=200):
     model = Sequential()
-    model.add(tf.keras.layers.InputLayer(input_shape=[batch_size, 64]))
+    # Adjusted input shape to (sequence_length, num_features)
+    model.add(tf.keras.layers.InputLayer(input_shape=(n_steps, 64)))
 
     for rate in dilation_rates:
-
         model.add(tf.keras.layers.Conv1D(
             filters=filters, kernel_size=kernel_size, padding="causal", activation="relu",
             dilation_rate=rate))
         model.add(tf.keras.layers.Conv1D(filters=14, kernel_size=1))
-
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(100, activation='relu'))
+    # Removed the extra input_shape parameter
     model.add(tf.keras.layers.Dense(3, activation='softmax'))
 
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-def convolutional(features,batch_size=512):
+def convolutional(features,batch_size=512,n_steps=200):
     model = Sequential()
-    model.add(tf.keras.layers.InputLayer(input_shape=[batch_size, 64]))
+    model.add(tf.keras.layers.InputLayer(input_shape=(n_steps, 64)))
 
     model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding="causal", activation="relu"))
     model.add(tf.keras.layers.BatchNormalization())
@@ -83,4 +83,3 @@ def convolutional(features,batch_size=512):
 
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
-
--- preprocessing.py
+++ preprocessing.py
@@ -62,7 +62,7 @@ def create_tf_dataset(basePath, n_steps, stride, batch_size):
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Create TF dataset with specified parameters.")
-    parser.add_argument("--batch_size", type=int, default=512, help="Batch size for the dataset.")
+    parser.add_argument("--batch_size", type=int, default=256, help="Batch size for the dataset.")
     parser.add_argument("--stride", type=int, default=100, help="Stride value for splitting time series.")
     parser.add_argument("--n_steps", type=int, default=200, help="Number of steps for each instance.")
     parser.add_argument("--tiny",type=float,default=0.05,help="Tiny dataset size")
@@ -96,7 +96,8 @@ if __name__ == "__main__":
     tiny_train_size = int(args.tiny * train_size)
     tiny_test_size = int(args.tiny * (dataset_size - train_size))
 
-    tiny_train_dataset = train_dataset.take(tiny_train_size)
-    tiny_test_dataset = test_dataset.take(tiny_test_size)
-    tf.data.experimental.save(tiny_test_dataset, 'tiny_test_dataset')
-    tf.data.experimental.save(tiny_train_dataset, 'tiny_train_dataset')
+    if not args.save:
+        tiny_train_dataset = train_dataset.take(tiny_train_size)
+        tiny_test_dataset = test_dataset.take(tiny_test_size)
+        tf.data.experimental.save(tiny_test_dataset, 'tiny_test_dataset')
+        tf.data.experimental.save(tiny_train_dataset, 'tiny_train_dataset')
