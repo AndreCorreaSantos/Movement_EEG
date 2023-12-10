63feb6775980c5437e3118e75634fa8039c66818
AndreCorreaSantos <acorreasantos2002@gmail.com>
2023-12-10 15:25:52 -0300

    adding final version

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # EEGMMIDB Dataset and Preprocessing
 
 ## Overview
@@ -35,17 +36,75 @@ Run the preprocessing script to generate preprocessed TensorFlow datasets:
 ```bash
 python preprocessing.py
 ```
+
 Create the tf dataset running
+
 ```
 python preprocessing.py
 ```
+
 It is possible to change the values passed to each variable, but any changes will have to be adjusted in the model loader in the master.ipynb.
-Example: 
+Example:
+
 ```
 python preprocessing.py --batch_size 2 --stride 100 --n_steps 700
 ```
 
 This script handles the necessary steps to preprocess the EEGMMIDB dataset and prepares it for further analysis.
+
+### Preprocessing Discussion
+
+#### 1. Data Loading and Label Encoding:
+
+The `getXYdata` function extracts EEG signals (X) and labels (y) from the raw EEG recordings. The labels are encoded using `LabelEncoder` to convert categorical labels into a numerical format.
+
+#### 2. Time Series Splitting:
+
+The `splitTimeSeries` function segments the time series data into instances with a specified number of steps (`n_steps`) and a specified stride. Choosing appropriate values for these parameters is crucial for subsequent analysis.
+
+#### 3. Generator Function:
+
+The `generator_function` iterates over EEG recordings of different individuals and exams using MNE to read raw EEG data. It calls `getXYdata` and `splitTimeSeries` to yield segmented instances for training.
+
+#### 4. TensorFlow Dataset Creation:
+
+The `create_tf_dataset` function converts the generator function into a TensorFlow dataset, specifying the output signature for X and y.
+
+#### 5. Command-Line Arguments:
+
+Command-line arguments such as `--batch_size`, `--stride`, and `--n_steps` impact the preprocessing pipeline. The optional `--save` argument allows for saving the full train and test datasets.
+
+#### 6. Dataset Splitting and Saving:
+
+The full dataset is split into training and testing sets with an 80-20 split. Optionally, users can save the datasets using `tf.data.experimental.save`.
+
+#### 7. Creating Smaller Datasets:
+
+The `--tiny` argument allows for creating smaller datasets (`tiny_train_dataset` and `tiny_test_dataset`) useful for faster testing during development.
+
+#### 8. Model comparison:
+
+The following models were trained and tested with 5% of the original dataset separated into batches of 512 instances each, with a stride of 100 between the data points and 200 time steps per data point. Each model was trained with 30 epochs and the training and testing (validation) data do not overlap.
+
+![Model Comparison](model_comparison.png)
+
+
+- Recurrent: a simple recurrent neural network was the first tested model, it clearly did not yield good results as it did underfitted severely and only predicted a single class on the test set.
+- Recurrent Complex: a more complex recurrent neural network. It did also underfitted.
+- Wavenet: Similar result to both recurrent models.
+- Convolutional: A medium sized convolutional neural network. It overfitted and was the only model to predict different classes regularly over the dataset, all the other models only predicted a single class.
+- Convolutional Regularized: In an attempt to stop the overfitting and improve performance over the test set the standard convolutional model was regulzarized using dropout layers, but it resulted in underfitting.
+
+## Conclusion:
+
+None of the models were able to predict the target variable with significant accuracy on the test set. The one which got the closest was the deep convolutional model.
+
+## Next steps
+
+The next steps will be:
+
+- improving pre-processing: Adding fast fourier transform to the pre-processing layer and generating images from the dataset will greatly add to the depth of information and should improve drastically the performance of the convolutional models.
+- Variational autoencoders: The state of the art papers in the field all use convolutional variational autoencoders to analyze and classify the resulting spectrograms from the fft, so these models might be the ideal tools to reach the envisioned goal of classifying motor imagery electro encephalogram datasets.
 
 ## References
 
@@ -54,5 +113,3 @@ The initial models used in this project are sourced from the following repositor
 [https://github.com/ageron/handson-ml3/blob/main/15_processing_sequences_using_rnns_and_cnns.ipynb](https://github.com/ageron/handson-ml3/blob/main/15_processing_sequences_using_rnns_and_cnns.ipynb)
 
 These models serve as a starting point for further development and experimentation with the EEGMMIDB dataset.
-
-https://github.com/ageron/handson-ml3/blob/main/15_processing_sequences_using_rnns_and_cnns.ipynb
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout, BatchNormalization, Bidirectional
+from tensorflow.keras.layers import LSTM, Dense, Dropout, BatchNormalization, Bidirectional, Conv1D, MaxPooling1D, Flatten, InputLayer
+from tensorflow.keras.regularizers import l2
 
 
-def recurrent(features,batch_size=512):
+def recurrent(features,n_steps=200):
     model = Sequential()
     model.add(LSTM(units=64, activation="tanh", return_sequences=False, input_shape=(None, features)))
     model.add(Dense(units=3, activation="softmax"))
@@ -30,56 +31,102 @@ def recurrent_2(features, lstm_units=64, dropout_rate=0.5):
 def wavenet(filters=32, kernel_size=2, dilation_rates=(1, 2, 4, 8) * 2,n_steps=200):
     model = Sequential()
     # Adjusted input shape to (sequence_length, num_features)
-    model.add(tf.keras.layers.InputLayer(input_shape=(n_steps, 64)))
+    model.add(InputLayer(input_shape=(n_steps, 64)))
 
     for rate in dilation_rates:
-        model.add(tf.keras.layers.Conv1D(
+        model.add(Conv1D(
             filters=filters, kernel_size=kernel_size, padding="causal", activation="relu",
             dilation_rate=rate))
-        model.add(tf.keras.layers.Conv1D(filters=14, kernel_size=1))
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(100, activation='relu'))
+        model.add(Conv1D(filters=14, kernel_size=1))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
     # Removed the extra input_shape parameter
-    model.add(tf.keras.layers.Dense(3, activation='softmax'))
+    model.add(Dense(3, activation='softmax'))
 
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-def convolutional(features,batch_size=512,n_steps=200):
+def convolutional(features, n_steps=200):
     model = Sequential()
-    model.add(tf.keras.layers.InputLayer(input_shape=(n_steps, 64)))
+    model.add(InputLayer(input_shape=(n_steps, features)))
 
-    model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding="causal", activation="relu"))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.MaxPooling1D(2))
+    model.add(Conv1D(filters=32, kernel_size=3, padding="causal", activation="relu"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
 
-    model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding="causal", activation="relu"))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.MaxPooling1D(2))
+    model.add(Conv1D(filters=64, kernel_size=3, padding="causal", activation="relu"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
 
-    model.add(tf.keras.layers.Conv1D(filters=128, kernel_size=3, padding="causal", activation="relu"))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.MaxPooling1D(2))
+    model.add(Conv1D(filters=128, kernel_size=3, padding="causal", activation="relu"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
 
-    model.add(tf.keras.layers.Conv1D(filters=256, kernel_size=3, padding="causal", activation="relu"))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.MaxPooling1D(2))
+    model.add(Conv1D(filters=256, kernel_size=3, padding="causal", activation="relu"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
 
-    model.add(tf.keras.layers.Conv1D(filters=256, kernel_size=3, padding="causal", activation="relu"))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.MaxPooling1D(2))
+    model.add(Conv1D(filters=256, kernel_size=3, padding="causal", activation="relu"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
 
-    model.add(tf.keras.layers.Conv1D(filters=128, kernel_size=3, padding="causal", activation="relu"))
-    model.add(tf.keras.layers.BatchNormalization())
+    model.add(Conv1D(filters=128, kernel_size=3, padding="causal", activation="relu"))
+    model.add(BatchNormalization())
 
-    model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding="causal", activation="relu"))
-    model.add(tf.keras.layers.BatchNormalization())
+    model.add(Conv1D(filters=64, kernel_size=3, padding="causal", activation="relu"))
+    model.add(BatchNormalization())
 
     # Flattening and final layers
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(100, activation='relu'))
-    model.add(tf.keras.layers.Dropout(0.5))
-    model.add(tf.keras.layers.Dense(3, activation='softmax'))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+def convolutional_2(features, n_steps=200, dropout_rate=0.5, l2_penalty=0.001):
+    model = Sequential()
+    model.add(InputLayer(input_shape=(n_steps, features)))
+
+    model.add(Conv1D(filters=32, kernel_size=3, padding="causal", activation="relu", kernel_regularizer=l2(l2_penalty)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv1D(filters=64, kernel_size=3, padding="causal", activation="relu", kernel_regularizer=l2(l2_penalty)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv1D(filters=128, kernel_size=3, padding="causal", activation="relu", kernel_regularizer=l2(l2_penalty)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv1D(filters=256, kernel_size=3, padding="causal", activation="relu", kernel_regularizer=l2(l2_penalty)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv1D(filters=256, kernel_size=3, padding="causal", activation="relu", kernel_regularizer=l2(l2_penalty)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(2))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv1D(filters=128, kernel_size=3, padding="causal", activation="relu", kernel_regularizer=l2(l2_penalty)))
+    model.add(BatchNormalization())
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv1D(filters=64, kernel_size=3, padding="causal", activation="relu", kernel_regularizer=l2(l2_penalty)))
+    model.add(BatchNormalization())
+    model.add(Dropout(dropout_rate))
+
+    # Flattening and final layers
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_regularizer=l2(l2_penalty)))
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(3, activation='softmax'))
 
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
